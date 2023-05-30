703efb3d2402c4c5c6390a8d974b0d2f987ebdee
Michela Meister <mcm377@cornell.edu>
2023-05-30 17:09:18 -0400

    now mid plots print to a single pdf file

--- pyro_model/code/plot_mid_data.py
+++ pyro_model/code/plot_mid_data.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
 import numpy as np
 import pandas as pd
 import sys
 
 NUM_ARGS = 3
+YMIN = 0
+YMAX = 1000
+
 n = len(sys.argv)
 if n != NUM_ARGS:
     print("Error: " + str(NUM_ARGS) + " arguments needed; only " + str(n) + " arguments given.")
@@ -14,15 +18,23 @@ df = pd.read_csv(read_fn)
 # assert that each MID has exactly one sample, one drug
 assert (df.groupby('MID').Sample.nunique() == 1).all()
 assert (df.groupby('MID').Drug.nunique() == 1).all()
-
+# create list of figures plotting mids growth curves
+fig_list = []
 for mid in df.MID.unique():
-    plt.clf()
+    f = plt.figure()
     m = df.loc[df.MID == mid]
     sample = m.Sample.unique()[0]
     drug = m.Drug.unique()[0]
     plt.title('MID: ' + str(mid) + ', Sample: ' + sample + ', Drug: ' + drug)
     plt.xlabel('Day')
-    plt.ylabel('Volume')
+    plt.ylabel('Volume mm^3')
+    ax = plt.gca()
+    ax.set_ylim([YMIN, YMAX])
     plt.plot(m.Day, m.Volume)
-    fn = write_dir + '/' + str(mid) + '.png'
-    plt.savefig(fn)
+    fig_list.append(f)
+    plt.close(f)
+# save all figures to one pdf
+p = matplotlib.backends.backend_pdf.PdfPages(write_dir + '/volume_vs_day.pdf')
+for f in fig_list:
+    p.savefig(f)
+p.close()
--- pyro_model/scripts/clean_and_split_data.sh
+++ pyro_model/scripts/clean_and_split_data.sh
@@ -2,13 +2,12 @@
 base_dir=results/"$(date +"%Y-%m-%d")"/clean_and_split_data
 
 mkdir -p $base_dir
-mkdir -p $base_dir/volume_vs_day
 mkdir -p $base_dir/split
 mkdir -p $base_dir/normality_plots
 
 python3 code/clean_data.py read_fn=data/welm_pdx.csv write_dir=$base_dir
 python3 code/add_mids.py read_fn=$base_dir/welm_pdx_clean.csv write_dir=$base_dir
-python3 code/plot_mid_data.py in_path=$base_dir/welm_pdx_clean_mid.csv write_dir=$base_dir/volume_vs_day
+python3 code/plot_mid_data.py in_path=$base_dir/welm_pdx_clean_mid.csv write_dir=$base_dir
 python3 code/add_duration.py in_path=$base_dir/welm_pdx_clean_mid.csv write_dir=$base_dir min_duration=21
 python3 code/split_train_test.py in_path=$base_dir/welm_pdx_clean_mid_volume.csv write_dir=$base_dir/split 
 python3 code/plot_normality.py in_path=$base_dir/split/train.pkl write_dir=$base_dir/normality_plots min_duration=21
