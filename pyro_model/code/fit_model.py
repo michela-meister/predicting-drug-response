f20e7b13f6f7625c0f7f91e71256d2f49b9b2d07
Michela Meister <mcm377@cornell.edu>
2023-06-05 18:41:46 -0400

    revising model, in progress

--- pyro_model/code/fit_model.py
+++ pyro_model/code/fit_model.py
@@ -62,7 +62,6 @@ smoke_test = ('CI' in os.environ)
 assert pyro.__version__.startswith('1.8.4')
 
 pyro.enable_validation(True)
-pyro.set_rng_seed(1)
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 # Set matplotlib settings
