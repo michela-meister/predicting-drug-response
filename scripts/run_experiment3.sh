e6c4f19b24b578f1f8663d79e50958e10beed615
Michela Meister <mcm377@cornell.edu>
2023-09-15 16:00:19 -0400

    add cross validation helpers

--- scripts/run_experiment3.sh
+++ scripts/run_experiment3.sh
@@ -14,13 +14,13 @@ do
 	method="raw"
 	writeDir="$baseDir/$method/$splitSeed"
 	mkdir -p "$writeDir"
-	#bsub -n 1 -W 0:30 -R 'span[hosts=1] rusage[mem=32]' -e "$writeDir/err.err" -o "$writeDir/out.out" \
+	bsub -n 1 -W 0:30 -R 'span[hosts=1] rusage[mem=32]' -e "$writeDir/err.err" -o "$writeDir/out.out" \
 	"$scriptDir/"pdx_expt.sh "$codeDir" "$method" "$dataFn" "$writeDir" "$foldFn" "$splitSeed" "$nSteps"
 
 	# Run script on transfer
 	method="transfer"
 	writeDir="$baseDir/$method/$splitSeed"
 	mkdir -p "$writeDir"
-	#bsub -n 1 -W 8:00 -R 'span[hosts=1] rusage[mem=32]' -e "$writeDir/err.err" -o "$writeDir/out.out" \
+	bsub -n 1 -W 8:00 -R 'span[hosts=1] rusage[mem=32]' -e "$writeDir/err.err" -o "$writeDir/out.out" \
 	"$scriptDir/"pdx_expt.sh "$codeDir" "$method" "$dataFn" "$writeDir" "$foldFn" "$splitSeed" "$nSteps"
 done
